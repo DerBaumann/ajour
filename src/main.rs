13307ff93ee1e52338dafff59617e986c7b53ace
DerBaumann <natur5210@gmail.com>
2026-05-12 13:31:20 +0200

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use axum::{Json, Router, http::StatusCode, routing::get};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Response {
+    message: &'static str,
+}
+
+async fn hello() -> (StatusCode, Json<Response>) {
+    let response = Response {
+        message: "Hello, World!",
+    };
+
+    (StatusCode::OK, Json(response))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new().route("/", get(hello));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+
+    println!("Listening on http://localhost:3000");
+    axum::serve(listener, app).await?;
+    Ok(())
+}
