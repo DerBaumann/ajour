26fe79637c11e955c2062230a8367e95202b0db3
DerBaumann <natur5210@gmail.com>
2026-05-13 10:31:38 +0200

    Add docker-compose for postgresdb, .env and set up sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use anyhow::Context;
 use axum::{Json, Router, http::StatusCode, routing::get};
+use clap::Parser;
 use serde::Serialize;
+use sqlx::postgres::PgPoolOptions;
+
+#[derive(clap::Parser)]
+struct Config {
+    #[clap(long, env)]
+    db_url: String,
+}
 
 #[derive(Serialize)]
 struct Response {
@@ -17,7 +25,18 @@ async fn hello() -> (StatusCode, Json<Response>) {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // This returns an error if the `.env` file doesn't exist, but that's not what we want
+    // since we're not going to use a `.env` file if we deploy this application.
+    dotenvy::dotenv().ok();
+
+    let config = Config::parse();
+
     let app = Router::new().route("/", get(hello));
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&config.db_url)
+        .await
+        .context("Failed to start db")?;
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
