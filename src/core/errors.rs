c1ca88e9dec09192d0ad0823090d833a0bcc7c3f
DerBaumann <natur5210@gmail.com>
2026-05-15 10:07:54 +0200

    Add AppError enum

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -1,5 +1,17 @@
 use axum::{http::StatusCode, response::IntoResponse};
 
+#[derive(Debug, thiserror::Error)]
+pub enum AppError {
+    #[error("Failed to render template")]
+    TemplateError(#[from] askama::Error),
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
+    }
+}
+
 pub struct AnyhowError(anyhow::Error);
 
 impl From<anyhow::Error> for AnyhowError {
--- src/core/router.rs
+++ src/core/router.rs
@@ -1,15 +1,14 @@
-use anyhow::Context;
 use askama::Template;
 use axum::{Router, http::StatusCode, response::Html, routing::get};
 
-use crate::core::{AppState, errors::AnyhowError};
+use crate::core::{AppState, errors::AppError};
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct HomeTemplate;
 
-async fn hello() -> Result<(StatusCode, Html<String>), AnyhowError> {
-    let tmpl = HomeTemplate.render().context("Failed to render template")?;
+async fn hello() -> Result<(StatusCode, Html<String>), AppError> {
+    let tmpl = HomeTemplate.render()?;
 
     Ok((StatusCode::OK, Html(tmpl)))
 }
