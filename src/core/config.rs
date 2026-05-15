7ada0057922e8b5901ef4f938bd2d0021ca10354
DerBaumann <natur5210@gmail.com>
2026-05-15 10:11:33 +0200

    Add default port

--- src/core/config.rs
+++ src/core/config.rs
@@ -1,9 +1,7 @@
-// TODO: Add default port
-
 #[derive(Debug, clap::Parser)]
 pub struct Config {
     #[arg(long, env)]
     pub database_url: String,
-    #[arg(long, short, env)]
+    #[arg(long, short, env, default_value_t = 8080)]
     pub port: u16,
 }
