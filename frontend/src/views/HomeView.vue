0bcb98dd4cf34d89d9a7900ce4127df486352bd9
DerBaumann <natur5210@gmail.com>
2026-05-17 19:34:52 +0200

    Replace vue with svelte to make things easier

--- frontend/src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <main>
-    <h1>Task Manager</h1>
-    <p>Hello, World!</p>
-  </main>
-</template>
--- frontend/src/views/tasks/TaskListView.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts"></script>
-
-<template>
-  <main>
-    <h1>Heutige Aufgaben</h1>
-  </main>
-</template>
