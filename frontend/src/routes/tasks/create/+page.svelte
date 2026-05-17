2ad507fa8830e43382c10fed8a4460997beb0eb8
DerBaumann <natur5210@gmail.com>
2026-05-17 20:31:21 +0200

    Add /tasks and /tasks/create to fe

---
+++ frontend/src/routes/tasks/create/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { resolve } from '$app/paths';
+</script>
+
+<h1>Neue Aufgabe</h1>
+
+<p>
+	<a href={resolve('/tasks')} class="btn preset-filled">Zurück </a>
+</p>
+
+<form action=""></form>
