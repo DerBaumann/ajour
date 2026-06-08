d50af896eec27d9ca9a1899c74885eba68fb828d
DerBaumann <natur5210@gmail.com>
2026-06-08 19:34:26 +0200

    Install better auth

---
+++ frontend/src/lib/server/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from './db';
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: 'pg'
+	}),
+	baseURL: 'http://localhost:3000/',
+	emailAndPassword: { enabled: true }
+});
