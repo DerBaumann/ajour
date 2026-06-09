60d2241675c9cf89b92ade7de735d689dfb4f6b1
DerBaumann <natur5210@gmail.com>
2026-06-09 13:31:42 +0200

    Fill in signin form action

--- frontend/src/routes/auth/signin/+page.server.ts
+++ frontend/src/routes/auth/signin/+page.server.ts
@@ -1,5 +1,8 @@
-import { redirect } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
+import { LoginCreds } from '$lib/auth/types';
+import { auth } from '$lib/server/auth';
+import { APIError } from 'better-auth';
 
 export const load: PageServerLoad = (event) => {
 	if (event.locals.user) {
@@ -10,5 +13,31 @@ export const load: PageServerLoad = (event) => {
 };
 
 export const actions: Actions = {
-	default: async ({ request }) => {}
+	default: async ({ request }) => {
+		const data = Object.fromEntries(await request.formData());
+		const { data: loginCreds, error } = LoginCreds.safeParse(data);
+		if (error) {
+			return fail(400, { message: error.message });
+		}
+
+		const { email, password } = loginCreds;
+
+		try {
+			await auth.api.signInEmail({
+				body: {
+					email,
+					password,
+					callbackURL: '/auth/verification-success'
+				}
+			});
+		} catch (error) {
+			if (error instanceof APIError) {
+				return fail(400, { message: error.message || 'Registration failed' });
+			}
+			return fail(500, { message: 'Unexpected error' });
+		}
+
+		// TODO: Change this later to /tasks
+		redirect(303, '/auth/protected');
+	}
 };
