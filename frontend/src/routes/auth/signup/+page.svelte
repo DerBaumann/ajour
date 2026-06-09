3b7b242e958be4b6a646009ed5da6b9aa4fa0de1
DerBaumann <natur5210@gmail.com>
2026-06-09 10:51:12 +0200

    Add registration form

--- frontend/src/routes/auth/signup/+page.svelte
+++ frontend/src/routes/auth/signup/+page.svelte
@@ -1,1 +1,15 @@
 <!-- TODO: Implement Signup -->
+<script lang="ts">
+	import Input from '$lib/components/Input.svelte';
+</script>
+
+<h1 class="h1">Registrierung</h1>
+
+<form class="w-full max-w-md space-y-4 p-4" method="POST">
+	<Input type="text" label="Nutzername" name="name" required />
+	<Input type="email" label="Email" name="email" placeholder="you@example.com" required />
+	<Input type="password" label="Passwort" name="password" required />
+	<Input type="password" label="Passwort wiederholen" name="password-repeat" required />
+
+	<button type="submit" class="btn preset-filled-primary-500">Registrieren</button>
+</form>
